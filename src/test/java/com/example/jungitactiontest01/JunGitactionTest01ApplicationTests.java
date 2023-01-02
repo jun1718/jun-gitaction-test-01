2a08d456615cc17cb1a275e3b307f73971ffd039
jun1718 <gbeovhsqhtka@naver.com>
2023-01-02 17:42:14 +0900

    Initial commit

---
+++ src/test/java/com/example/jungitactiontest01/JunGitactionTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jungitactiontest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunGitactionTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
